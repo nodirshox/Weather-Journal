85fc26358cb403755bd8466bcb1cfdc298da895b
nodirshox <n.ergashev@student.inha.uz>
2020-05-31 19:54:10 +0500

    Initial

---
+++ server.js
@@ -0,0 +1,39 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// Start up an instance of app
+const app = express()
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+app.get('/all', (req, res) => {
+    res.send(projectData)
+})
+
+app.post('/add', addWeather);
+
+function addWeather(req,res){
+    projectData.temp = req.body.temperature,
+    projectData.date = req.body.date,
+    projectData.res = req.body.response
+}
+// Setup Server
+app.listen(3000, running())
+
+function running() {
+    console.log("Server started on " + 3000)
+}
---
+++ website/app.js
@@ -0,0 +1,61 @@
+/* Global Variables */
+const key = 'a05d9352534ed5de4f19dd4cdbc7de64';
+const url = 'http://api.openweathermap.org/data/2.5/weather?zip='
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', action);
+var feeling = document.getElementsByClassName('myInput');
+
+const postData = async ( url = '/add', data = {})=>{
+  const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)     
+  });
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch(error) {
+    console.log("Error details: ", error);
+  }
+};
+
+
+function action(d){
+  const newCountry =  document.getElementById('zip').value;
+  getCountry(url, newCountry, key)
+  .then(function(data) {
+    console.log(feeling)
+    postData('/add', {temperature: data.main.temp, date: newDate, response: feeling[0].value})
+
+    updateUI() //update UI
+  })
+}
+
+const getCountry = async (url, country, key)=>{
+  const res = await fetch(url + country + '&appid=' + key)
+  try {
+    const data = await res.json();
+    return data;
+  } catch(error) {
+    console.log("Error details: ", error);
+  }
+}
+
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try{
+    const allData = await request.json();
+    document.getElementById('temp').innerHTML = "Temperature: " + allData.temp;
+    document.getElementById('date').innerHTML = "Date: " + allData.date;
+    document.getElementById('content').innerHTML = "Feeling: " + allData.res;
+  } catch(error) {
+    console.log("Error details: ", error);
+  }
+}
